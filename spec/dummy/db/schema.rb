2a1671aa29c3d54d706cfdc32db29e665ac6fbc2
Jesse Trimble <jesseltrimble@gmail.com>
2012-07-03 14:54:19 -0600

    Schema

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -11,10 +11,12 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120627000003) do
+ActiveRecord::Schema.define(:version => 20120703205153) do
 
   create_table "habberdash_dashboard", :force => true do |t|
-    t.text "configuration"
+    t.text     "configuration"
+    t.datetime "created_at",    :null => false
+    t.datetime "updated_at",    :null => false
   end
 
 end
