d871dba085f007b7eca12a4383632acf5361b0ac
Jesse Trimble <jlowelltrim@gmail.com>
2012-07-05 14:47:01 -0600

    Remove old migration file

--- db/migrate/20120703204302_create_habberdash_dashboard.rb
+++
@@ -1,8 +0,0 @@
-class CreateHabberdashDashboard < ActiveRecord::Migration
-  def change
-    create_table :habberdash_dashboard do |t|
-      t.text :configuration
-      t.timestamps
-    end
-  end
-end
